5c65866f9cc4542ec4d28c2c76d5fad7a0ca1276
Kesav Kumar Jothichandran <kesav.kumarj@perficient.com>
2026-06-04 11:49:24 +0530

    failure case #2 - changed - to + in sub function

--- test_calc.py
+++ test_calc.py
@@ -6,7 +6,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
     def test_subtract(self):
         self.assertEqual(subtract(5, 2), 3)
-        self.assertEqual(subtract(0, 1), 0)
+        self.assertEqual(subtract(0, 1), -1)
     def test_multiply(self):
         self.assertEqual(multiply(4, 3), 12)
         self.assertEqual(multiply(-2, 5), -10)
